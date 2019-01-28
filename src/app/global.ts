cac51efc2b616d01eea9007f1d6d8041e911a2af
andrewsmaia@gmail.com <andrewsmaia@gmail.com>
2019-01-28 15:25:27 -0200

    Melhorias

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,2 +1,3 @@
-export const enderecoAPI: string="https://localhost:5001/api/";
+export const enderecoAPI: string="http://INNOVATIVO.KINGHOST.NET/api/";
 export const podeExibirMensagens: boolean=false;
+
