7fceb0f4db9e9222944e07691b550f52e96396ec
andrew.maia <andrew.maia@talktelecom.com.br>
2019-03-08 17:26:36 -0300

    Refatoração

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -31,7 +31,5 @@ export class LoginComponent {
   }
 
 
-  logout() {
-    this.authService.logout();
-  }
+
 }
--- src/app/shared/forbidden-name.directive.ts
+++
@@ -1,9 +0,0 @@
-import { AbstractControl,ValidatorFn } from '@angular/forms';
-
-/** A hero's name can't match the given regular expression */
-export function forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: any} | null => {
-      const forbidden = nameRe.test(control.value);
-      return forbidden ?  null : {'forbiddenName': {value: control.value}} ;
-    };
-  }
