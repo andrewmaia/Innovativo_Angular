7613eecdd5066384857290531aca1eb4a83bf6f2
Andrew Maia <andrewsmaia@gmail.com>
2024-01-30 12:06:30 -0300

    ajustes

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,4 +1,2 @@
-export const enderecoAPI: string="http://INNOVATIVO.KINGHOST.NET/api/";
-//export const enderecoAPI: string="https://localhost:5001/api/";
-
-
+//export const enderecoAPI: string="http://INNOVATIVO.KINGHOST.NET/api/";
+export const enderecoAPI: string = "https://localhost:5001/api/";
